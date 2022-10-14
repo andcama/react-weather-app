8317e9373aa387923dc35cee41364502f1299e8b
andcama <andcamachoa@gmail.com>
2022-10-14 17:01:52 -0600

    first Commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e3b44d152dmshf89bff7256b48a6p141a8ajsn83d79110145f',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL="https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY="c6c4e827d2940fa4f8dc4e8ad5700c6b";
